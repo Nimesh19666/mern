24573badbccd34ce7746b33bc0eda5da3bb841a7
Nimesh19666 <gujarinimesh-cmpn@atharvacoe.ac.in>
2025-09-02 02:24:06 +0530

    3rd day push

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,28 +1,13 @@
 const express = require("express")
 const router = express.Router()
+const {getGoals,postGoals,updateGoals,deleteGoals} = require("../controllers/goalControllers") 
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        message:'Get Goals'
-    })
-})
+router.get('/', getGoals)
 
-router.post('/', (req, res) => {
-    res.status(200).json({
-        message:'Create Goals'
-    })
-})
+router.post('/', postGoals)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({
-        message:`Update Goals ${req.params.id}`
-    })
-})
+router.put('/:id', updateGoals)
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({
-        message:`Delete Goals ${req.params.id}`
-    })
-})
+router.delete('/:id', deleteGoals)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const dotenc = require('dotenv').config()
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 8000
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
 
 app.listen(port,() => console.log(`Server Started in port ${port}`))
