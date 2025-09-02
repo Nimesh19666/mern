7328f672577259674fb3c6a4f4a58a23baf63bc4
Nimesh19666 <gujarinimesh-cmpn@atharvacoe.ac.in>
2025-09-02 09:20:05 +0530

    4th commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const port = process.env.PORT || 8000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDb = require('./config/db')
 
+connectDb()
 const app = express()
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
